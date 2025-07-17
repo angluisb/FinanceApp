6f5c6d6dcec26a35951df86658f8a96755ba1cbe
angluisb <luis042k@hotmail.com>
2025-07-16 20:05:27 -0700

    add UserController

---
+++ src/main/java/com/angluisb/finance_app/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.angluisb.finance_app.controller;
+
+import com.angluisb.finance_app.dto.request.UserRequest;
+import com.angluisb.finance_app.dto.response.UserResponse;
+import com.angluisb.finance_app.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("create")
+    public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(request));
+    }
+}
