ad40e12b838551815e61dce9bfaa39fa898ae6da
angluisb <luis042k@hotmail.com>
2025-07-17 20:54:32 -0700

    add some methods

--- src/main/java/com/angluisb/finance_app/service/UserService.java
+++ src/main/java/com/angluisb/finance_app/service/UserService.java
@@ -2,28 +2,11 @@ package com.angluisb.finance_app.service;
 
 import com.angluisb.finance_app.dto.request.UserRequest;
 import com.angluisb.finance_app.dto.response.UserResponse;
-import com.angluisb.finance_app.entity.Enum.RolesType;
 import com.angluisb.finance_app.entity.User;
-import com.angluisb.finance_app.mapper.UserMapper;
-import com.angluisb.finance_app.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final  UserRepository userRepository;
-    private final  UserMapper userMapper;
-    private final PasswordEncoder passwordEncoder;
-
-
-
-    public UserResponse createUser(UserRequest request) {
-        User user = userMapper.toEntity(request);
-        user.setRole(RolesType.USER);
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        User savedUser = userRepository.save(user);
-        return userMapper.toUserResponse(savedUser);
-    }
+public interface UserService {
+    public UserResponse createUser(UserRequest user);
+    public User getById(Long id);
+    void delete(Long id);
+    public UserResponse updateUser(UserRequest user, Long id);
 }
