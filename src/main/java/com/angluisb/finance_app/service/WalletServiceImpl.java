d2d4e9c2a7509543868acdb2a78761ef8ab5530c
angluisb <luis042k@hotmail.com>
2025-07-17 20:50:02 -0700

    add WalletServiceImpl

---
+++ src/main/java/com/angluisb/finance_app/service/WalletServiceImpl.java
@@ -0,0 +1,48 @@
+package com.angluisb.finance_app.service;
+
+import com.angluisb.finance_app.dto.request.WalletRequest;
+import com.angluisb.finance_app.dto.response.WalletResponse;
+import com.angluisb.finance_app.entity.User;
+import com.angluisb.finance_app.entity.Wallet;
+import com.angluisb.finance_app.mapper.UserMapper;
+import com.angluisb.finance_app.mapper.WalletMapper;
+import com.angluisb.finance_app.repository.WalletRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WalletServiceImpl implements WalletService {
+
+    private final WalletMapper walletMapper;
+    private final WalletRepository walletRepository;
+    private final UserServiceImpl userService;
+
+
+
+    @Override
+    public WalletResponse createWallet(WalletRequest walletRequest) {
+        Wallet wallet = walletMapper.toEntity(walletRequest);
+        Wallet savedWallet = walletRepository.save(wallet);
+
+        User fullUser = userService.getById(walletRequest.getUserId());
+        savedWallet.setUser(fullUser);
+
+        return walletMapper.toWalletResponse(savedWallet);
+    }
+
+    @Override
+    public WalletResponse getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public WalletResponse updateWallet(WalletRequest walletRequest, Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteWallet(Long id) {
+
+    }
+}
