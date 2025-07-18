1a4e854f21729cdaec31a8afbd059d9b43040548
angluisb <luis042k@hotmail.com>
2025-07-17 21:38:08 -0700

    add getById

--- src/main/java/com/angluisb/finance_app/service/WalletService.java
+++ src/main/java/com/angluisb/finance_app/service/WalletService.java
@@ -2,10 +2,11 @@ package com.angluisb.finance_app.service;
 
 import com.angluisb.finance_app.dto.request.WalletRequest;
 import com.angluisb.finance_app.dto.response.WalletResponse;
+import com.angluisb.finance_app.entity.Wallet;
 
 public interface WalletService {
     WalletResponse createWallet(WalletRequest walletRequest);
-    WalletResponse getById(Long id);
+    Wallet getById(Long id);
     WalletResponse updateWallet(WalletRequest walletRequest, Long id);
     void deleteWallet(Long id);
 }
