9ce00ca9d8fcc0bbcd0d2b8bac52d3463f3fcaeb
angluisb <luis042k@hotmail.com>
2025-07-17 20:53:52 -0700

    add builder properties

--- src/main/java/com/angluisb/finance_app/dto/summary/UserSummary.java
+++ src/main/java/com/angluisb/finance_app/dto/summary/UserSummary.java
@@ -1,11 +1,13 @@
 package com.angluisb.finance_app.dto.summary;
 
 import com.angluisb.finance_app.entity.Enum.RolesType;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class UserSummary {
     private Long id;
     private String firstName;
--- src/main/java/com/angluisb/finance_app/dto/summary/WalletSummary.java
+++ src/main/java/com/angluisb/finance_app/dto/summary/WalletSummary.java
@@ -1,9 +1,11 @@
 package com.angluisb.finance_app.dto.summary;
 
 import com.angluisb.finance_app.entity.Enum.CurrencyType;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class WalletSummary {
     private Long id;
     private String name;
