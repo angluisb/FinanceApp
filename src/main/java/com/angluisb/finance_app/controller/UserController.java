bd1c5b6b194ca546035b416b17108437e5308165
angluisb <luis042k@hotmail.com>
2025-07-17 20:55:37 -0700

    rename methods in UserController

--- src/main/java/com/angluisb/finance_app/controller/UserController.java
+++ src/main/java/com/angluisb/finance_app/controller/UserController.java
@@ -2,12 +2,11 @@ package com.angluisb.finance_app.controller;
 
 import com.angluisb.finance_app.dto.request.UserRequest;
 import com.angluisb.finance_app.dto.response.UserResponse;
-import com.angluisb.finance_app.service.UserService;
+import com.angluisb.finance_app.service.UserServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
+    private final UserServiceImpl userService;
 
     @PostMapping("create")
     public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest request){
