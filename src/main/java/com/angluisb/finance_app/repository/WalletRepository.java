b7c08d6626ab1648cc5d348788f398271a0c34c6
angluisb <luis042k@hotmail.com>
2025-09-29 19:26:23 -0700

    WalletServiceImpl completed

--- src/main/java/com/angluisb/finance_app/repository/WalletRepository.java
+++ src/main/java/com/angluisb/finance_app/repository/WalletRepository.java
@@ -2,10 +2,14 @@ package com.angluisb.finance_app.repository;
 
 import com.angluisb.finance_app.entity.User;
 import com.angluisb.finance_app.entity.Wallet;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
     List<Wallet> findAllByUser(User user);
+
+    boolean existsByNameAndUser(@NotNull(message = "Name is required") @Size(min = 3, max = 50, message = "name size must be between 3 and 50") String name, User user);
 }
--- src/main/java/com/angluisb/finance_app/service/WalletServiceImpl.java
+++ src/main/java/com/angluisb/finance_app/service/WalletServiceImpl.java
@@ -5,18 +5,20 @@ import com.angluisb.finance_app.dto.response.WalletResponse;
 import com.angluisb.finance_app.dto.update.WalletUpdate;
 import com.angluisb.finance_app.entity.User;
 import com.angluisb.finance_app.entity.Wallet;
+import com.angluisb.finance_app.exception.BusinessException;
 import com.angluisb.finance_app.exception.ResourceNotFoundException;
 import com.angluisb.finance_app.mapper.WalletMapper;
-import com.angluisb.finance_app.repository.UserRepository;
 import com.angluisb.finance_app.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class WalletServiceImpl implements WalletService {
 
     private final WalletMapper walletMapper;
@@ -26,54 +28,40 @@ public class WalletServiceImpl implements WalletService {
 
 
     @Override
+    @Transactional
     public WalletResponse createWallet(WalletRequest walletRequest) {
+        User user = userService.getById(walletRequest.getUserId());
+        if (walletRepository.existsByNameAndUser(walletRequest.getName(), user)){
+            throw new BusinessException("Wallet with this name already exists");
+        }
+
         Wallet wallet = walletMapper.toEntity(walletRequest);
-
-        User fullUser = userService.getById(walletRequest.getUserId());
-        wallet.setUser(fullUser);
-
+        wallet.setUser(user);
         Wallet savedWallet = walletRepository.save(wallet);
-
         return walletMapper.toWalletResponse(savedWallet);
     }
 
     @Override
     public List<WalletResponse> findAllWalletsByUser(Long userId) {
         User user = userService.getById(userId);
-
-        if (user == null) {
-            throw new ResourceNotFoundException("User not found");
-        }
-
         List<Wallet> walletList = walletRepository.findAllByUser(user);
-
         return walletMapper.toWalletResponse(walletList);
     }
 
     @Override
     public Wallet getById(Long id) {
         return walletRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Wallet not found with id:" +id));
+                .orElseThrow(() -> new ResourceNotFoundException("Wallet not found with id: " + id));
     }
 
     @Override
+    @Transactional
     public WalletResponse updateWallet(WalletUpdate walletRequest, Long id) {
-        if (id == null) {
-            throw new IllegalArgumentException("wallet id must not be null");
-        }
+        Wallet walletToUpdate = getById(id);
 
-        Optional<Wallet> optionalWallet = walletRepository.findById(id);
-
-        if (optionalWallet.isEmpty()){
-            throw new ResourceNotFoundException("Wallet not found with id: "+ id);
-        }
-
-        Wallet walletToUpdate = optionalWallet.get();
-
-        walletToUpdate.setBalance(walletRequest.getBalance());
-        walletToUpdate.setName(walletRequest.getName());
-        walletToUpdate.setCurrency(walletRequest.getCurrency());
-
+        Optional.ofNullable(walletRequest.getBalance()).ifPresent(walletToUpdate::setBalance);
+        Optional.ofNullable(walletRequest.getName()).ifPresent(walletToUpdate::setName);
+        Optional.ofNullable(walletRequest.getCurrency()).ifPresent(walletToUpdate::setCurrency);
 
         return walletMapper.toWalletResponse(walletRepository.save(walletToUpdate));
     }
@@ -81,15 +69,9 @@ public class WalletServiceImpl implements WalletService {
 
 
     @Override
+    @Transactional
     public void deleteWallet(Long id) {
-        if (id == null) {
-            throw new IllegalArgumentException("wallet id must not be null");
-        }
-
-        if (walletRepository.findById(id).isEmpty()) {
-            throw new ResourceNotFoundException("Not found Wallet with id: "+ id);
-        }
-
-        walletRepository.deleteById(id);
+        Wallet wallet = getById(id);
+        walletRepository.delete(wallet);
     }
 }
